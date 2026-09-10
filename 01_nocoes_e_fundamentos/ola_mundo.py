7c9f55c03ddb0ca2fabcec3c927d1881b1f239e6
LuizVictor7 <secundoluizvictor@gmail.com>
2026-09-10 16:15:55 -0300

    teste com print e input

--- 01_nocoes_e_fundamentos/ola_mundo.py
+++ 01_nocoes_e_fundamentos/ola_mundo.py
@@ -1,2 +1,11 @@
+# exercicio com print e input
 print ("Luiz victor")
 print("Olá mundo!")
+
+nome = input("digite seu nome:")
+mes = input("digite seu mês de aniversário:")
+dia = input("Digite o dia que nasceu")
+ano = input("digite o ano que nasceu:")
+#prt(f"Seu nome é", nome, ", seu mês de aniversário é", mes,"do dia", dia, "do ano",ano)
+
+print(f"seu nome é {nome}você nasceu no dia {dia} do mês {mes} do ano {ano}")
