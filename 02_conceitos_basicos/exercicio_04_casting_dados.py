b5f36c4dfba1e453940b131ffbc8b962c374aee1
LuizVictor7 <secundoluizvictor@gmail.com>
2026-09-17 20:34:30 -0300

    atividade mao na massa

--- 02_conceitos_basicos/exercicio_04_casting_dados.py
+++ 02_conceitos_basicos/exercicio_04_casting_dados.py
@@ -11,6 +11,7 @@ Imprima a idade calculada com uma mensagem personalizada.
 
 # TODO: Desenvolva o algoritmo abaixo:
 ano_nascimento = input("Digite o seu ano de nascimento: ")
+nome = input("Digite seu nome")
 ano_nascimento = int(ano_nascimento)
 idade_2026 = 2026 - ano_nascimento
-print(f"Em 2026, você derá {idade_2026} anos. Parabéns pelas alegrias que conquistou até aqui! e pelas que ainda virão!")
+print(f"Em 2026. Você, {nome} derá {idade_2026} anos. Parabéns pelas alegrias que conquistou até aqui! e pelas que ainda virão!")
