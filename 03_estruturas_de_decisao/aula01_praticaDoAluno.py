3f944b6995a7f8d202f0b75d4ec50d54b4b1c554
LuizVictor7 <secundoluizvictor@gmail.com>
2026-09-17 16:09:15 -0300

    mão na massa

---
+++ 03_estruturas_de_decisao/aula01_praticaDoAluno.py
@@ -0,0 +1,7 @@
+# TODO: Implemente a expressão de validação
+media_aluno = 7.5
+frequencia_percentual = 80
+
+# Crie a variável aprovado com a expressão lógica
+aprovado = (media_aluno >= 6) and (frequencia_percentual >= 70)
+print("Status de aprovação:", aprovado)
---
+++ 03_estruturas_de_decisao/aula02_praticaDoAluno.py
@@ -0,0 +1,8 @@
+# TODO: Escreva o programa do par ou ímpar aqui
+numero = int(input("Digite um número inteiro: "))
+
+# Escreva a estrutura if / else
+if numero % 2 == 0:
+    print ("O número é par")
+else:
+    print("O número é impar")
