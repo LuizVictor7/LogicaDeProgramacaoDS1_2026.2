605a54ad54ddf5648eb9470e65577566017b667b
LuizVictor7 <secundoluizvictor@gmail.com>
2026-09-17 14:34:05 -0300

    bug exercicio

--- 02_conceitos_basicos/exercicio_03_conta_sushi.py
+++ 02_conceitos_basicos/exercicio_03_conta_sushi.py
@@ -13,4 +13,4 @@ Crie um programa que:
 valor_consumido = float(input("Digite o valor total consumido no restaurante (em R$): "))
 taxa_servico = valor_consumido * 0.10
 valor_final = valor_consumido + taxa_servico
-print(f"O valor final da conta que deverá ser pagoé: R$ {valor_final:.1f}")
+print(f"O valor final da conta que deverá ser pago é: R$ {valor_final:.1f}")
