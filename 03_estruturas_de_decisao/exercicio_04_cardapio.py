16b4c799cf30757230664b63eeda185a479cc23c
LuizVictor7 <secundoluizvictor@gmail.com>
2026-09-21 13:47:24 -0300

    ajustes na logica e manuntenção nos exercicios

--- 03_estruturas_de_decisao/exercicio_04_cardapio.py
+++ 03_estruturas_de_decisao/exercicio_04_cardapio.py
@@ -1,18 +1,18 @@
-"""
-EXERCÍCIO 04: Cardápio da Lanchonete
-Disciplina: Lógica de Programação com Python
+# """
+# EXERCÍCIO 04: Cardápio da Lanchonete
+# Disciplina: Lógica de Programação com Python
 
-TABELA:
-1 - Cachorro Quente: R$ 4.00
-2 - X-Salada: R$ 4.50
-3 - X-Bacon: R$ 5.00
-4 - Torrada Simples: R$ 2.00
-5 - Refrigerante: R$ 1.50
+# TABELA:
+# 1 - Cachorro Quente: R$ 4.00
+# 2 - X-Salada: R$ 4.50
+# 3 - X-Bacon: R$ 5.00
+# 4 - Torrada Simples: R$ 2.00
+# 5 - Refrigerante: R$ 1.50
 
-ENUNCIADO:
-Leia o código do item e a quantidade consumida.
-Calcule e mostre o total a pagar.
-"""
+# ENUNCIADO:
+# Leia o código do item e a quantidade consumida.
+# Calcule e mostre o total a pagar.
+# """
 
 # TODO: Desenvolva o algoritmo abaixo:
 codigo_item = int(input("Digite o código do item (1 a 5):"))
